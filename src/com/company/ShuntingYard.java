10a2970c373f7a7b43b293bc6e57be181a969a4e
Joey Uriarte <Joey.Uriarte@saintandrews.net>
2021-09-17 10:48:30 -0400

    Initial commit

---
+++ src/com/company/ShuntingYard.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class ShuntingYard {
+
+    public static String infixToPostFix(String expression) {
+        return null;
+    }
+
+    public static String postFixToInfix(String expression) {
+        return null;
+    }
+
+    public static boolean isValidExpression(String expressionToCheck) {
+        return false;
+    }
+
+    public static double evaluatePostFix(String expression) {
+        return 0;
+    }
+}
